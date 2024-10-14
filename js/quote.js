c14666c5ec24bd0c8d99afb6330ff457f2c3ea78
Feyfa <fisikamodern00@gmail.com>
2024-10-14 21:53:37 +0700

    add window.onload

--- js/quote.js
+++ js/quote.js
@@ -24,51 +24,52 @@ cardContainerEls.forEach((cardContainerEl) => {
 });
 
 
-
-document.addEventListener('scroll', () => {
-    cardContainerEls.forEach((cardContainerEl, index) => {
-        // ambil viewport, ini adalah tinggi browser terhadapat tinggi monitor
-        const heightViewport = window.innerHeight;
-        // ambil posisi scrollY
-        const positionScrollY = window.scrollY;
-
-
-        // saya menemukan bug, animasi tidak berjalan, dan saya menduga kode ini lah penyebabbnya
-        // saya merasa jika kode dibawah dilakukan dengan sangat cepat, dan tanpa ada delay, maka animasi seketika error tidak berjalan
-        // oleh karena itu saya memberikan delay 50ms, walaupun 10ms itu cepat, tapi membantu untuk mengatasi bug ini
-        setTimeout(() => {
-            // jika heightViewport + positionScrollY >= positionBottom_cardContainerEls[index]
-            // maka kita telah sampai di tempat elemen berada
-            // maka kita berikan dia class show, agar animasi untuk memunculkan cardContainerEl bekerja
-            if(heightViewport + positionScrollY >= positionBottom_cardContainerEls[index]) {
-                cardContainerEl.classList.remove('hidden');
-                cardContainerEl.classList.add('show');
-
-                // untuk elemen img
-                cardContainerEl.firstElementChild.classList.remove('hidden');
-                cardContainerEl.firstElementChild.classList.add('show');
-
-                // untuk elemen p
-                cardContainerEl.lastElementChild.classList.remove('hidden');
-                cardContainerEl.lastElementChild.classList.add('show');
-            } else {
-                cardContainerEl.classList.remove('show');
-                cardContainerEl.classList.add('hidden');
-
-                // untuk elemen img
-                cardContainerEl.firstElementChild.classList.remove('show');
-                cardContainerEl.firstElementChild.classList.add('hidden');
-
-                // untuk elemen p
-                cardContainerEl.lastElementChild.classList.remove('show');
-                cardContainerEl.lastElementChild.classList.add('hidden');
-            }
-        }, 0);
-
-
+window.onload = () => {
+    document.addEventListener('scroll', () => {
+        cardContainerEls.forEach((cardContainerEl, index) => {
+            // ambil viewport, ini adalah tinggi browser terhadapat tinggi monitor
+            const heightViewport = window.innerHeight;
+            // ambil posisi scrollY
+            const positionScrollY = window.scrollY;
+    
+    
+            // saya menemukan bug, animasi tidak berjalan, dan saya menduga kode ini lah penyebabbnya
+            // saya merasa jika kode dibawah dilakukan dengan sangat cepat, dan tanpa ada delay, maka animasi seketika error tidak berjalan
+            // oleh karena itu saya memberikan delay 50ms, walaupun 10ms itu cepat, tapi membantu untuk mengatasi bug ini
+            setTimeout(() => {
+                // jika heightViewport + positionScrollY >= positionBottom_cardContainerEls[index]
+                // maka kita telah sampai di tempat elemen berada
+                // maka kita berikan dia class show, agar animasi untuk memunculkan cardContainerEl bekerja
+                if(heightViewport + positionScrollY >= positionBottom_cardContainerEls[index]) {
+                    cardContainerEl.classList.remove('hidden');
+                    cardContainerEl.classList.add('show');
+    
+                    // untuk elemen img
+                    cardContainerEl.firstElementChild.classList.remove('hidden');
+                    cardContainerEl.firstElementChild.classList.add('show');
+    
+                    // untuk elemen p
+                    cardContainerEl.lastElementChild.classList.remove('hidden');
+                    cardContainerEl.lastElementChild.classList.add('show');
+                } else {
+                    cardContainerEl.classList.remove('show');
+                    cardContainerEl.classList.add('hidden');
+    
+                    // untuk elemen img
+                    cardContainerEl.firstElementChild.classList.remove('show');
+                    cardContainerEl.firstElementChild.classList.add('hidden');
+    
+                    // untuk elemen p
+                    cardContainerEl.lastElementChild.classList.remove('show');
+                    cardContainerEl.lastElementChild.classList.add('hidden');
+                }
+            }, 0);
+    
+    
+        })
     })
-})
-// ===============CODE===============
+    // ===============CODE===============
+}
 
 
 
